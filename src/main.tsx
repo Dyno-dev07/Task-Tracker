74acc5b52121b3a544937d1118caf1ace4df1919
[dyad] <git@dyad.sh>
2025-10-22 16:30:15 +0800

    [dyad] Removed theme animation - wrote 2 file(s), deleted 2 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -3,25 +3,11 @@ import App from "./App.tsx";
 import "./globals.css";
 import { ThemeProvider } from "./components/ThemeProvider.tsx";
 import { FontProvider } from "./context/FontContext.tsx";
-import { ThemeAnimationProvider, useThemeAnimation } from "./context/ThemeAnimationContext.tsx";
-import ThemeChangeAnimator from "./components/ThemeChangeAnimator.tsx";
-
-// This component will consume the ThemeAnimationContext
-const AppWithThemeAnimationKey = () => {
-  const { themeChangeCount } = useThemeAnimation();
-
-  return (
-    <FontProvider> {/* FontProvider needs to wrap App */}
-      <App key={themeChangeCount} />
-      <ThemeChangeAnimator />
-    </FontProvider>
-  );
-};
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-    <ThemeAnimationProvider> {/* ThemeAnimationProvider must wrap AppWithThemeAnimationKey */}
-      <AppWithThemeAnimationKey />
-    </ThemeAnimationProvider>
+    <FontProvider>
+      <App />
+    </FontProvider>
   </ThemeProvider>
 );
