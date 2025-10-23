eebbddab79502de00cdc663c807285c814fe694a
[dyad] <deanneagir@gmail.com>
2025-10-23 10:00:28 +0800

    [dyad] Wrapped header in a div to fix parsing error - wrote 1 file(s)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,23 +16,25 @@ const Header: React.FC<HeaderProps> = ({ onOpenMobileSidebar, onToggleDesktopSid
   const isMobile = useIsMobile();
 
   return (
-    <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 justify-between">
-      {isMobile ? (
-        <Button variant="ghost" size="icon" onClick={onOpenMobileSidebar} className="md:hidden">
-          <Menu className="h-6 w-6" />
-          <span className="sr-only">Toggle sidebar</span>
-        </Button>
-      ) : (
-        <Button variant="ghost" size="icon" onClick={onToggleDesktopSidebar} className="hidden md:flex">
-          {isDesktopSidebarOpen ? <ChevronLeft className="h-6 w-6" /> : <ChevronRight className="h-6 w-6" />}
-          <span className="sr-only">Toggle desktop sidebar</span>
-        </Button>
-      )}
-      <div className="flex-grow"></div> {/* Spacer */}
-      {!isMobile && (
-        <CreateTaskDialog />
-      )}
-    </header>
+    <div> {/* Added a wrapper div here */}
+      <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 justify-between">
+        {isMobile ? (
+          <Button variant="ghost" size="icon" onClick={onOpenMobileSidebar} className="md:hidden">
+            <Menu className="h-6 w-6" />
+            <span className="sr-only">Toggle sidebar</span>
+          </Button>
+        ) : (
+          <Button variant="ghost" size="icon" onClick={onToggleDesktopSidebar} className="hidden md:flex">
+            {isDesktopSidebarOpen ? <ChevronLeft className="h-6 w-6" /> : <ChevronRight className="h-6 w-6" />}
+            <span className="sr-only">Toggle desktop sidebar</span>
+          </Button>
+        )}
+        <div className="flex-grow"></div> {/* Spacer */}
+        {!isMobile && (
+          <CreateTaskDialog />
+        )}
+      </header>
+    </div> // Closing wrapper div
   );
 };
 
